8898aef3855d36f3bf3985aea5b17c6518f772e3
3m4nr3su <hillaryotieno97@gmail.com>
2022-03-16 19:53:02 +0000

    k

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,14 +6,19 @@
 /**
  *positive_or_negative - checks if +/-
  * @i: the int to test
+ * Return: on success 0
  */
 
 int positive_or_negative(int i);
 
 /**
  * largest_number - prints largest no. out of 3
+ * @a: a no.
+ * @b: a no.
+ * @c: a no.
+ * Return: on success 0
  */
 
-int largest_number(int, int, int);
+int largest_number(int a, int b, int c);
 
 #endif
