d881007b8020402b3f963afb88824d7603767f33
3m4nr3su <hillaryotieno97@gmail.com>
2022-03-16 18:40:08 +0000

    k

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+
+/**
+ *positive_or_negative - checks if +/-
+ * @i: the int to test
+ */
+
+int positive_or_negative(int i);
+
+#endif
---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
+ */
+
+int positive_or_negative(int n)
+{
+	n = 0;
+
+	if (n == 0)
+		printf("%d is zero\n", n);
+
+	return (0);
+}
