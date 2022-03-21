6a600acc1d206afa176ab7c7cac2d72340a10eb7
3m4nr3su <hillaryotieno97@gmail.com>
2022-03-21 10:30:41 +0000

    k

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * reset_to_98 - takes a pointer to an int & updates the value it points to to 98
+ * reset_to_98 - takes a pointer & updates the value it points to to 98
  *
- * @v: value
+ * @n: value
  * Return: new value
  */
 
