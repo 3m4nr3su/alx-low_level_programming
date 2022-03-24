a88c707ba9287b4ca7fe7a7f637e7a4bfecca39f
3m4nr3su <hillaryotieno97@gmail.com>
2022-03-24 10:37:52 +0000

    k

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,30 +1,11 @@
 #include "main.h"
 
 /**
- * _strlen - returns string length
- * @s: string
- *
- * Return: length
- */
-
-int _strlen(char *s)
-{
-	int len = 0;
-
-	while (*s != '\0')
-	{
-		len++;
-		s++;
-	}
-
-	return (len);
-}
-
-/**
  * _strncpy - should work exactly like strncpy
  * @dest: ination
  * @src: source
- * n: bytes?
+ * @n: bytes?
+ * Return: dest
  */
 
 char *_strncpy(char *dest, char *src, int n)
@@ -32,7 +13,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src [i];
+		dest[i] = src[i];
 	for (; i < n; i++)
 		dest[i] = '\0';
 
